9c41088eaa97b1562c158fb8c4f1a0a7df5ac45f
rachelpanella-hub <76072338+rachelpanella-hub@users.noreply.github.com>
2020-12-17 09:16:00 -0600

    Update javafunctions.js

--- javafunctions.js
+++ javafunctions.js
@@ -49,19 +49,20 @@ scrollToTopButton.onclick = function(e) {
 var modal = document.getElementById('myModal');
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById('myImg');
-var modalImg = document.getElementById("img01");
+var img = $('.myImg');
+var modalImg = $("#img01");
 var captionText = document.getElementById("caption");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-}
+$('.myImg').click(function(){
+    modal.style.display = "block";
+    var newSrc = this.src;
+    modalImg.attr('src', newSrc);
+    captionText.innerHTML = this.alt;
+});
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() { 
+span.onclick = function() {
   modal.style.display = "none";
 }
