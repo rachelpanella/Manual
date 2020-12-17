91bd0cf2b770c50ca06f55513c0ee6c91f7cf36b
rachelpanella-hub <76072338+rachelpanella-hub@users.noreply.github.com>
2020-12-17 09:35:46 -0600

    Update javafunctions.js

--- javafunctions.js
+++ javafunctions.js
@@ -45,26 +45,28 @@ scrollToTopButton.onclick = function(e) {
   scrollToTop();
 }
 
-// Get the modal
-var modal = document.getElementById("myModal");
-var i;
-
-var img = document.getElementsByClassName("myImg");
+// create references to the modal...
+var modal = document.getElementById('myModal');
+// to all images -- note I'm using a class!
+var images = document.getElementsByClassName('myImages');
+// the image in the modal
 var modalImg = document.getElementById("img01");
+// and the caption in the modal
+var captionText = document.getElementById("caption");
 
- for(i=0;i< img.length;i++)
-   {    
-    img[i].onclick = function(){
-
+// Go through all of the images with our custom class
+for (var i = 0; i < images.length; i++) {
+  var img = images[i];
+  // and attach our click listener for this image.
+  img.onclick = function(evt) {
     modal.style.display = "block";
-       modalImg.src = this.src;
-
- }
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
 }
 
 var span = document.getElementsByClassName("close")[0];
 
-
-span.onclick = function() { 
-   modal.style.display = "none";
+span.onclick = function() {
+  modal.style.display = "none";
 }
